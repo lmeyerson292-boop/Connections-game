4b67c432391d4ae370c8e43a91d9475b1ffa65f8
lmeyerson292-boop <lmeyerson292@milkenschool.org>
2026-05-21 17:22:14 -0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Connections Game",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
